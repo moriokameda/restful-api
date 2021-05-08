bd54b7c42542c10891371656a25dc359b9e475cf
Morio Kameda <mkame19941031@gmail.com>
2021-05-09 01:39:11 +0900

    spring周りの更新

---
+++ src/test/kotlin/com/moriokameda/restfulApi/RestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moriokameda.restfulApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
